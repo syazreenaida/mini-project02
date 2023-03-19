73cfef94ce54db98ed0be41c879f2e7b62ca169e
syazreen <syazreenaida@gmail.com>
2023-03-19 20:29:16 +0800

    shit

---
+++ src/main/java/mini_project02/mini_project/controllers/AuthController.java
@@ -0,0 +1,68 @@
+package mini_project02.mini_project.controllers;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.SpotifyHttpManager;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    //TODO: must change the api URI in here and spotify app
+    private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/api/get-user");
+    public String code = "";
+    private static final String CLIENT_ID = "15a30563c68e414ca7d5e8a12baabc1b";
+    private static final String CLIENT_SECRET = "662b61c45fcf42c38a5fbb87b9a25212";
+
+    public static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setClientId(CLIENT_ID)
+            .setClientSecret(CLIENT_SECRET)
+            .setRedirectUri(redirectUri)
+            .build();
+
+
+    @GetMapping("login-user")
+    @ResponseBody
+    public String spotifyLogin(){
+        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
+            .scope("user-read-private, user-read-email, user-top-read")
+            .show_dialog(true)
+            .build();
+        final URI uri = authorizationCodeUriRequest.execute();
+    return uri.toString();
+
+    }
+
+    public String getSpotifyUserCode(@RequestParam("code") String userCode, HttpServletResponse response) throws IOException{
+        code = userCode;
+        AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code)
+                .build();
+        
+        try{
+            final AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+
+            // Set Access and refresh token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+            
+        }catch(IOException | SpotifyWebApiException | org.apache.hc.core5.http.ParseException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        response.sendRedirect("http://localhost:3000/top-artists");
+        return userCode;
+    }
+}
---
+++ src/main/java/mini_project02/mini_project/controllers/SpotifyApiController.java
@@ -0,0 +1,38 @@
+package mini_project02.mini_project.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wrapper.spotify.model_objects.specification.Artist;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.requests.data.personalization.simplified.GetUsersTopArtistsRequest;
+
+import static mini_project02.mini_project.controllers.AuthController.spotifyApi;
+
+@RestController
+@RequestMapping("/api")
+public class SpotifyApiController {
+    
+    @GetMapping(value = "user-top-artists")
+    public Artist[] getUserToArtists(){
+        
+        final GetUsersTopArtistsRequest getUsersTopArtistsRequest = spotifyApi.getUsersTopArtists()
+                .time_range("medium_term")
+                .limit(10)
+                .offset(5)
+                .build();
+
+        try{
+            final Paging<Artist> artistPaging =getUsersTopArtistsRequest.execute();
+
+            //return as JSON 
+            return artistPaging.getItems();
+
+        }catch(Exception e){
+            System.out.println("Something went wrong!\n" +e.getMessage());;
+        }
+        return new Artist[0];
+}
+    }
+    
